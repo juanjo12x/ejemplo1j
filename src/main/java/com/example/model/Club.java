f3be9c642874bcff0d94b25b58cb984d5651a986
Juanjo <xshinobi12x@yahoo.pe>
2016-04-05 23:02:23 -0500

    changing

---
+++ src/main/java/com/example/model/Club.java
@@ -0,0 +1,7 @@
+package com.example.model;
+
+/**
+ * Created by Juan Tenorio on 5/4/2016.
+ */
+public class Club {
+}
---
+++ src/main/java/com/example/repository/ClubRepository.java
@@ -0,0 +1,7 @@
+package com.example.repository;
+
+/**
+ * Created by Juan Tenorio on 5/4/2016.
+ */
+public class ClubRepository {
+}
